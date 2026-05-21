075f4ed49ee74bdd123b802203b736eedfa322b9
nithishpothuru <p.nithish564@@gmail.com>
2026-05-21 21:58:36 +0530

    feat(scopeMemebers): implemented scope member list using scope.ts file with  listAssignableMembers functions and scopeEngine.ts file as helper in lib folder which has Groups and added new function is added in employees.ts helper file listEmployeesByOrg.

---
+++ convex/lib/scopeEngine.ts
@@ -0,0 +1,44 @@
+export function buildGroups(input: {
+    organization: string[];
+    project: string[];
+    track: string[];
+    task: string[];
+  }) {
+    const taskSet = new Set(input.task);
+    const trackSet = new Set(input.track);
+    const projectSet = new Set(input.project);
+  
+    const task: string[] = [];
+    const track: string[] = [];
+    const project: string[] = [];
+    const organization: string[] = [];
+  
+    const all = new Set([
+      ...input.organization,
+      ...input.project,
+      ...input.track,
+      ...input.task,
+    ]);
+  
+    for (const userId of all) {
+      if (taskSet.has(userId)) {
+        task.push(userId);
+      } 
+      else if (trackSet.has(userId)) {
+        track.push(userId);
+      } 
+      else if (projectSet.has(userId)) {
+        project.push(userId);
+      } 
+      else {
+        organization.push(userId);
+      }
+    }
+  
+    return {
+      task,
+      track,
+      project,
+      organization,
+    };
+  }
